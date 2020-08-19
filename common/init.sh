ac036c6065b9fa32b458b6638c26c9bcedda7300
usa.yimian.xyz <git@usa.yimian.xyz>
2020-08-19 09:47:32 -0400

    r

--- common/init.sh
+++ common/init.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+<<<<<<< HEAD
 chmod +x /etc/rc.d/rc.local
 for fileName in $(ls $(dirname $0)/init.d)
+=======
+cd $(dirname $0)
+rm -f /etc/rc.d/rc.local
+cp ./init.d/rc.d/rc.local /etc/rc.d/
+chmod +x /etc/rc.d/rc.local
+for fileName in $(ls ./init.d)
+>>>>>>> 49ec88c... rc
 do
     if [ ! -d $(dirname $0)/init.d/$fileName  ]; then
         echo sh $fileName>>/etc/rc.d/rc.local
