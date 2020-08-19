81632f702a8b4ae00dab1555a8c2afd35db9e566
usa.yimian.xyz <git@usa.yimian.xyz>
2020-08-19 09:52:44 -0400

    merge

--- common/init.sh
+++ common/init.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-<<<<<<< HEAD
-chmod +x /etc/rc.d/rc.local
-<<<<<<< HEAD
-for fileName in $(ls $(dirname $0)/init.d)
-=======
 cd $(dirname $0)
 rm -f /etc/rc.d/rc.local
 cp ./init.d/rc.d/rc.local /etc/rc.d/
 chmod +x /etc/rc.d/rc.local
 for fileName in $(ls ./init.d)
->>>>>>> 49ec88c... rc
-=======
-cd $(dirname $0)
-for fileName in $(ls ./init.d)
->>>>>>> fb54644687bbdb39c10fe73b704adede0e067d82
 do
     if [ ! -d $(pwd)/init.d/$fileName  ]; then
         echo sh $(pwd)/init.d/$fileName>>/etc/rc.d/rc.local
